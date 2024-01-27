2156f10302aaad503e3040f8b0e6c9b536dce56b
parveenkaradi <praveen.karadi@logicmonitor.com>
2024-01-27 12:02:50 +0530

    postcode and then address search

--- src/main/java/com/kbytes/liveaddress/service/SearchService.java
+++ src/main/java/com/kbytes/liveaddress/service/SearchService.java
@@ -1,7 +1,6 @@
 package com.kbytes.liveaddress.service;
 
-import co.elastic.clients.elasticsearch._types.query_dsl.MultiMatchQuery;
-import co.elastic.clients.elasticsearch._types.query_dsl.TextQueryType;
+import co.elastic.clients.elasticsearch._types.query_dsl.*;
 import com.kbytes.liveaddress.persistence.elasticsearch.models.ESLiveAddress;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.elasticsearch.client.elc.NativeQuery;
@@ -9,6 +8,8 @@ import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 @Service("SearchService")
@@ -20,14 +21,33 @@ public class SearchService {
         if (userInput.length() < 3) {
             return List.of(); // Return an empty list for input less than 3 characters
         }
+        Query query = null;
+        if(isPostcode(userInput)) {
+            query = new MatchBoolPrefixQuery.Builder()
+                    .query(userInput)
+                    .field("postcode").build()._toQuery();
+        } else {
+            query = new MatchQuery.Builder()
+                    .query(userInput)
+                    .field("fulladdress").build()._toQuery();
+        }
         NativeQuery searchQuery = NativeQuery.builder()
-                .withQuery(new MultiMatchQuery.Builder()
-                        .query(userInput)
-                        .fields("postcode","fulladdress")
-                        .type(TextQueryType.BestFields).build()._toQuery())
+                .withQuery(query)
                 .build();
 
         return elasticsearchOperations.search(searchQuery, ESLiveAddress.class).stream()
                 .map(sh -> sh.getContent()).collect(Collectors.toList());
+    }
+
+    /**
+     * logic to determine if the query is in postcode format
+     * @param query
+     * @return true if matches elase false
+     */
+    private boolean isPostcode(String query) {
+        String regex = "\\b[A-Z]{1,2}\\d{1,2}(?:\\s\\d[A-Z]{1,2})?\\b";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(query);
+        return matcher.find() && query.length() <= 8;
     }
 }
